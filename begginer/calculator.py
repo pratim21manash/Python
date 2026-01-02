691b878ad5f52cd4929c8facddb0e4b58e10a3ed
manash <pratimkashyapm@gmail.com>
2026-01-02 13:31:23 +0530

    calculator.

---
+++ begginer/calculator.py
@@ -0,0 +1,7 @@
+a = 12
+b = 4
+
+print("The sum of two numbers is", a + b)
+print("The difference of two number is", a - b)
+print("The product of two number is", a - b)
+print("The divison of two number is", a / b)
