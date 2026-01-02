afb6bf3fe29edefbc2762113b2174e3938177511
manash <pratimkashyapm@gmail.com>
2026-01-02 13:25:13 +0530

    user input

---
+++ begginer/userInput.py
@@ -0,0 +1,8 @@
+# name = input("Enter your first name:")
+# print("Welcome", name)
+
+
+
+# user age input
+age = int(input("Enter your age:"))
+print("You will be", age + 5, "years old after 5 years")
