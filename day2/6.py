b4a45129208d152f3191e38ec8e36a0a1d63e8d8
manash <pratimkashyapm@gmail.com>
2026-01-03 10:54:04 +0530

    loop

---
+++ day2/6.py
@@ -0,0 +1,30 @@
+# even or odd checker
+
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+
+# Voting eligibility checker
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+
+
+# positive negative or zero checker
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
--- learnFlask/app.py
+++ learnFlask/app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return "Hello user! This is my first Flask app."
 
+@app.route("/about")
+def about():
+    return "This is the about page of my first Flask app."
+
+@app.route("/contact")
+def contact():
+    return "This is the contact page of my first Flask app."
+
 if __name__ == "__main__":
     app.run(debug=True)
 
