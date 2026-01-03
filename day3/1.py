f0df7fb94156bd79c8d184699207dc5f96e693a1
manash <pratimkashyapm@gmail.com>
2026-01-03 11:23:30 +0530

    basic loop

---
+++ day3/1.py
@@ -0,0 +1,15 @@
+# Print numbers from 1 to 5
+
+# for i in range(1, 6):
+#     print(i)
+
+
+# Print numbers from 5 to 1 in revers order
+# for i in range(5, 0, -1):
+#     print(i)
+
+
+# Print only even numbers from 1 to 10
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
