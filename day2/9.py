74a7d392fd56a0a92336010a086ab3be9e53cf09
manash <pratimkashyapm@gmail.com>
2026-01-03 11:14:12 +0530

    done

---
+++ day2/9.py
@@ -0,0 +1,23 @@
+# Grade calculator
+
+# marks = int(input("Enter your marks: "))
+
+# if marks >= 90:
+#     print("Grade A")
+
+# elif marks >= 75:
+#     print("Grade B")
+
+# elif marks >= 60:
+#     print("Grade C")
+
+# else:
+#     print("Fail")
+
+
+
+
+# Temperature converter
+celsius = float(input("Enter temperature in celcius:"))
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit is:", fahrenheit)
